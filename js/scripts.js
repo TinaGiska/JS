5252b53aa963fea22bdd843697e813fc436b39dd
Tina Giska <tina.tylicka@gmail.com>
2018-03-14 14:44:52 +0100

    Add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var a = 1,
+    h = 2,
+    triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
